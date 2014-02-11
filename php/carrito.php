e341964193a69715dc6ffb9019a1b4210751be2a
alumno <alumno@2A5PC18>
2014-02-11 14:44:10 +0100

    practica 3 daw

--- php/carrito.php
+++ php/carrito.php
@@ -1,10 +1,4 @@
 <?php
-// Archivo: siguiente.php
-require_once 'carrito.php';
-
-session_start();
-
 $carrito = $_SESSION['carrito'];
 $nombre=$carrito.numero;
-echo 'Hola, mi nombre es '.$nombre ;
 ?>
